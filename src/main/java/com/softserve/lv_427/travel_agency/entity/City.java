0a0c7133f7c890c5dafa002f3008f12ed4b5b43d
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-07-25 11:40:08 +0300

    add lombock and entity for country and city

--- src/main/java/com/softserve/lv_427/travel_agency/entity/City.java
+++ src/main/java/com/softserve/lv_427/travel_agency/entity/City.java
@@ -1,4 +1,21 @@
 package com.softserve.lv_427.travel_agency.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "City")
+@Data
+@NoArgsConstructor
 public class City {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name = "city_name")
+  private String cityName;
+
+  @ManyToOne private Country country;
 }
--- src/main/java/com/softserve/lv_427/travel_agency/entity/Country.java
+++ src/main/java/com/softserve/lv_427/travel_agency/entity/Country.java
@@ -1,4 +1,23 @@
 package com.softserve.lv_427.travel_agency.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Country")
+@Data
+@NoArgsConstructor
 public class Country {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name = "country_name")
+  private String countryName;
+
+  @OneToMany(mappedBy = "id")
+  private List<City> cities;
 }
