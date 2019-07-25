9cbb063fcedeba4cbcb5cac12b5442371c032b35
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-07-25 17:13:02 +0300

    test controller

--- src/main/java/com/softserve/lv_427/travel_agency/controller/AboutController.java
+++ src/main/java/com/softserve/lv_427/travel_agency/controller/AboutController.java
@@ -1,4 +1,23 @@
 package com.softserve.lv_427.travel_agency.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class AboutController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView mainPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("about");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public ModelAndView allFilms() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("about");
+        return modelAndView;
+    }
 }
