85d7bfa142fc4596ad229af2639681aa6199b756
ZakharSk <sklzakhar@gmail.com>
2019-08-12 22:29:13 +0300

    s

--- src/main/java/com/softserve/lv_427/travel_agency/exception/FieldNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.lv_427.travel_agency.exception;
-
-public class FieldNotFoundException extends BaseException {
-    public FieldNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/softserve/lv_427/travel_agency/service/AboutService.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.lv_427.travel_agency.service;
-
-import com.softserve.lv_427.travel_agency.dto.AboutDto;
-
-import java.sql.SQLException;
-
-public interface AboutService {
-  AboutDto getAboutDto() throws SQLException;
-}
