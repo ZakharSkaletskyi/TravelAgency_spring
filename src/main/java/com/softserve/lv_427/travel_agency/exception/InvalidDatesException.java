beb29c6b94063b6b3c154e595ff3896f6ded6296
ZakharSk <sklzakhar@gmail.com>
2019-08-13 20:00:10 +0300

    s

--- src/main/java/com/softserve/lv_427/travel_agency/exception/InvalidDatesException.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.lv_427.travel_agency.exception;
-
-public class InvalidDatesException extends BaseException {
-    public InvalidDatesException(String message) {
-        super(message);
-    }
-}
